f5b81bfd2922cd59a41aa253380dc66f3e533529
Sucker <vertihvost60@gmail.com>
2022-03-14 09:17:31 +0300

    The first application

--- Bong_Shope/urls.py
+++ Bong_Shope/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Bong_Shope.views import home
-
+from Bong_Shope.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home)
+    path('home/', home),
+    path('about/', about),
 ]
